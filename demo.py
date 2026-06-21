19bb4bd859c88e1900fa1584a3b3b45a13e47dc7
Aryan12Yadav <aryanyadav0507wp@gmail.com>
2026-06-21 12:02:27 +0530

    add nb

--- demo.py
+++ demo.py
@@ -1,4 +1,13 @@
 #below code is to check the logging config
 from src.logger import logging
-
+from src.exception import MyException
 logging.debug("This is a debug message")
+import sys
+
+try:
+    a = 1+"2"
+
+except Exception as e:
+    logging.info(e)
+    raise MyException(e,sys) from e
+
